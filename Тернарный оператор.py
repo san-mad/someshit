9ac8111d9df6a43f1bf11ecfa8b90d3087d6a009
Sanch <sannsi699@gmail.com>
2025-08-02 13:10:42 +0500

    123

--- Тернарный оператор.py
+++ Тернарный оператор.py
@@ -1,3 +1,16 @@
 password = input("Введите пароль: ")
-password = 123456 if password == "123456" else "Неверный пароль"
+if password == str("123456"):
+    print("Пароль принят")
+else:
+    print("Неверный пароль")
 #короче хуеня ебаная ваша тернарная операция
+
+for symbol in password:
+    if symbol == '1':
+        print("Символ 1 найден в пароле")
+    elif symbol == '2':
+        print("Символ 2 найден в пароле")
+    elif symbol == '3':
+        print("Символ 3 найден в пароле")
+    else:
+        print("Другой символ найден в пароле")
