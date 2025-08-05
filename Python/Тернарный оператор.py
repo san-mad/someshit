6aa98b9b682fe4a50f3348d93cfc7ab1672533fd
Sanch <sannsi699@gmail.com>
2025-08-05 10:21:32 +0500

    change

--- Python/Тернарный оператор.py
+++ Python/Тернарный оператор.py
@@ -1,39 +1,16 @@
-# Посмотрите на определение функции, которая возвращает модуль переданного числа:
-'''
-def abs(number):
-    if number >= 0:
-        return number
-    return -number
+password = input("Введите пароль: ")
+if password == str("123456"):
+    print("Пароль принят")
+else:
+    print("Неверный пароль")
+#короче хуеня ебаная ваша тернарная операция
 
-# Но можно записать более лаконично. Для этого справа от return должно быть выражение, 
-# но if — это инструкция, а не выражение. В Python есть конструкция, которая работает как if-else, но считается выражением. 
-# Она называется тернарный оператор — единственный оператор в Python, который требует три операнда:
-
-def abs(number):
-    return number if number >= 0 else -number
-
-#Общий паттерн выглядит так: <expression on true> if <predicate> else <expression on false>.
-
-#Давайте перепишем начальный вариант get_type_of_sentence() аналогично.
-
-#Было:
-
-def get_type_of_sentence(sentence):
-    last_char = sentence[-1]
-    if last_char == '?':
-        return 'question'
-    return 'normal'
-
-#Стало:
-
-def get_type_of_sentence(sentence):
-    last_char = sentence[-1]
-    return 'question' if last_char == '?' else 'normal'
-
-print(get_type_of_sentence('Hodor'))   # => normal
-print(get_type_of_sentence('Hodor?'))  # => question
-'''
-
-
-def flip_flop(flip):
-    return 'flop' if flip == 'flip' else 'flip'
+for symbol in password:
+    if symbol == '1':
+        print("Символ 1 найден в пароле")
+    elif symbol == '2':
+        print("Символ 2 найден в пароле")
+    elif symbol == '3':
+        print("Символ 3 найден в пароле")
+    else:
+        print("Другой символ найден в пароле")
